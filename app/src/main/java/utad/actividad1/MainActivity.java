219cff04e16b39f3a9039e9582a55dee8182f188
daniel <daniel_sephirot@hotmail.com>
2016-11-12 13:39:16 +0100

    commit en el que ponemos los editText en no editables al principio y hacemos que cuando pincheos en el boton editar se puedan editar

--- app/src/main/java/utad/actividad1/MainActivity.java
+++ app/src/main/java/utad/actividad1/MainActivity.java
@@ -3,13 +3,18 @@ package utad.actividad1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    public Button btnHolaMundo;
-    public TextView txtHolaMundo;
+
     public Button btnVolver;
     public Button btnEditar;
+    public EditText nombre;
+    public EditText email;
+    public EditText tlf;
+    public EditText direccion;
+
     MainActivityController mainActivityController;
 
     @Override
@@ -23,7 +28,24 @@ public class MainActivity extends AppCompatActivity {
         btnEditar=(Button)this.findViewById(R.id.button2);
 
 
+        nombre=(EditText)this.findViewById(R.id.editText);
+        email=(EditText)this.findViewById(R.id.editText2);
+        tlf=(EditText)this.findViewById(R.id.editText3);
+        direccion=(EditText)this.findViewById(R.id.editText4);
 
+
+
+        nombre.setEnabled(false);
+        email.setEnabled(false);
+        tlf.setEnabled(false);
+        direccion.setEnabled(false);
+
+
+
+        nombre.setOnClickListener(mainActivityController);
+        email.setOnClickListener(mainActivityController);
+        tlf.setOnClickListener(mainActivityController);
+        direccion.setOnClickListener(mainActivityController);
         btnVolver.setOnClickListener(mainActivityController);
         btnEditar.setOnClickListener(mainActivityController);
 
--- app/src/main/java/utad/actividad1/MainActivityController.java
+++ app/src/main/java/utad/actividad1/MainActivityController.java
@@ -11,21 +11,30 @@ public class MainActivityController implements View.OnClickListener,View.OnFocus
     boolean blEditable=false;
 
 
+
     MainActivityController(MainActivity vista){
         this.vista=vista;
     }
     @Override
+
+
+
     public void onClick(View view) {
 
         if(view.getId()== vista.btnEditar.getId()){
             vista.btnEditar.setText("cancelar");
             vista.btnVolver.setText("guardar");
+            vista.btnEditar.setTextColor( -65536);
+            vista.nombre.setEnabled(true);
+            vista.email.setEnabled(true);
+            vista.tlf.setEnabled(true);
+            vista.direccion.setEnabled(true);
         }
     }
 
     @Override
     public void onFocusChange(View view, boolean b) {
-
+     
 
     }
 }
